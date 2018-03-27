4ba8022f9620f957072bfb4f8b3148854699ee29
jianzi0307 <jianzi0307@gmail.com>
2018-03-27 11:22:10 +0800

    index.vue

--- server/lib/models/index.js
+++ server/lib/models/index.js
@@ -1,28 +1,35 @@
 const mongoose = require('mongoose');
 const isProd = process.env.NODE_ENV === 'production'
-const { settings } = require('../../../utils');
+const {settings} = require('../../../utils');
 
+var options = {
+  server: {
+    auto_reconnect: true,
+    poolSize: 10
+  },
+  useMongoClient: true
+};
 if (!isProd) {
-    mongoose.connect("mongodb://localhost/doracms2", { useMongoClient: true });
+  mongoose.connect("mongodb://localhost/doracms2", options);
 } else {
-    mongoose.connect('mongodb://' + settings.USERNAME + ':' + settings.PASSWORD + '@' + settings.HOST + ':' + settings.PORT + '/' + settings.DB + '', { useMongoClient: true });
+  mongoose.connect('mongodb://' + settings.USERNAME + ':' + settings.PASSWORD + '@' + settings.HOST + ':' + settings.PORT + '/' + settings.DB + '', options);
 }
 
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
 db.once('open', () => {
-    console.log('连接数据成功')
+  console.log('连接数据成功')
 })
 
 db.on('error', function (error) {
-    console.error('Error in MongoDb connection: ' + error);
-    mongoose.disconnect();
+  console.error('Error in MongoDb connection: ' + error);
+  mongoose.disconnect();
 });
 
 db.on('close', function () {
-    console.log('数据库断开，重新连接数据库');
-    // mongoose.connect(config.url, {server:{auto_reconnect:true}});
+  console.log('数据库断开，重新连接数据库');
+  // mongoose.connect(config.url, {server:{auto_reconnect:true}});
 });
 
 
