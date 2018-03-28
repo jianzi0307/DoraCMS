f484deb556ff6a5643d9ef6f85e92b0491df133f
jianzi0307 <jianzi0307@gmail.com>
2018-03-28 15:28:34 +0800

    10M

--- src/manage/components/content/contentForm.vue
+++ src/manage/components/content/contentForm.vue
@@ -240,12 +240,12 @@ export default {
       const isJPG = file.type === "image/jpeg";
       const isPNG = file.type === "image/png";
       const isGIF = file.type === "image/gif";
-      const isLt2M = file.size / 1024 / 1024 < 2;
+      const isLt2M = file.size / 1024 / 1024 < 10;
       if (!isJPG && !isPNG && !isGIF) {
-        this.$message.error("上传头像图片只能是 JPG,PNG,GIF 格式!");
+        this.$message.error("上传图片只能是 JPG,PNG,GIF 格式!");
       }
       if (!isLt2M) {
-        this.$message.error("上传头像图片大小不能超过 2MB!");
+        this.$message.error("上传图片大小不能超过 10MB!");
       }
       return (isJPG || isPNG || isGIF) && isLt2M;
     },
